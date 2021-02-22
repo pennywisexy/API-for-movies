cdad49e80941172dda82843fd4feb6c703d80877
Aliaksandr Snitko <pennywisexy@gmail.com>
2021-02-22 19:47:49 +0300

    moved store to mongodb

--- index.js
+++ index.js
@@ -1,32 +1,20 @@
 const express = require("express");
-const fs = require("fs");
+const mongoose = require("mongoose");
 const app = express();
 const jsonParser = express.json();
+const Movie = require('./models/movie')
 app.use(express.static(__dirname + "/public"));
 
-const filePath = "data.json";
-
-app.get("/api/movies", function (req, res) {
-  const content = fs.readFileSync(filePath, "utf8");
-  const dataMovies = JSON.parse(content);
-  const movies = dataMovies.categories[0].videos;
+app.get("/api/movies", async function (req, res) {
+  const movies = await Movie.find();
   res.send(movies);
 });
 
-app.get("/api/movies/:id", function (req, res) {
+app.get("/api/movies/:id", async function (req, res) {
+  console.log(req.params)
+
   const id = req.params.id;
-  const content = fs.readFileSync(filePath, "utf8");
-  const dataMovies = JSON.parse(content);
-  const movies = dataMovies.categories[0].videos;
-  let movie = null;
-
-  for (let i = 0; i < movies.length; i++) {
-    if (+movies[i].id === +id) {
-      movie = movies[i];
-      break;
-    }
-  }
-
+  const movie = await Movie.findById(id);
   if (movie) {
     res.send(movie);
   } else {
@@ -34,77 +22,58 @@ app.get("/api/movies/:id", function (req, res) {
   }
 });
 
-app.post("/api/movies", jsonParser, function (req, res) {
+app.post("/api/movies", async function (req, res) {
+  // console.log(req.body)
 
   if (!req.body) return res.sendStatus(400);
 
-  const movieDescription = req.body.description;
-  const movieSources = req.body.sources;
-  const movieSubtitle = req.body.subtitle;
-  const movieThumb = req.body.thumb;
-  const movieTitle = req.body.title;
-  let movie = {
-    description: movieDescription,
-    sources: [movieSources],
-    subtitle: movieSubtitle,
-    thumb: movieThumb,
-    title: movieTitle,
-  };
-  let data = fs.readFileSync(filePath, "utf8");
-  const dataMovies = JSON.parse(data);
-  const movies = dataMovies.categories[0].videos;
-
-  const id = Math.max.apply(
-    Math,
-    movies.map(function (item) {
-      return +item.id;
-    })
-  );
-
-  movie.id = id + 1;
-
-  movies.unshift(movie);
-  data = JSON.stringify(dataMovies);
-
-  fs.writeFileSync("data.json", data);
-  res.send(movie);
-});
-
-app.put("/api/movies", jsonParser, function (req, res) {
-  if (!req.body) return res.sendStatus(400);
-
-  const movieDescription = req.body.description;
-  const movieSources = req.body.sources;
-  const movieSubtitle = req.body.subtitle;
-  const movieThumb = req.body.thumb;
-  const movieTitle = req.body.title;
-  const id = req.body.id;
-  let data = fs.readFileSync(filePath, "utf8");
-  const dataMovies = JSON.parse(data);
-  const movies = dataMovies.categories[0].videos;
-  let movie;
-
-  for (let i = 0; i < movies.length; i++) {
-    if (+movies[i].id === +id) {
-      movie = movies[i];
-      break;
-    }
-  }
-
-  if (movie) {
-    movie.description = movieDescription;
-    movie.sources = movieSources;
-    movie.subtitle = movieSubtitle;
-    movie.thumb = movieThumb;
-    movie.title = movieTitle;
-
-    data = JSON.stringify(dataMovies);
-    fs.writeFileSync("data.json", data);
+  const movie = new Movie({
+    title: req.body.title,
+    description: req.body.description,
+    sources: req.body.sources,
+    subtitle : req.body.subtitle,
+    thumb: req.body.thumb
+  })
+  try {
+    await movie.save();
     res.send(movie);
-  } else {
-    res.status(404).send(movie);
+  } catch (e) {
+    console.log(e);
   }
 });
-app.listen(3000, function () {
-  console.log("Waiting to connect...");
+
+app.post('/api/movies/edit', jsonParser, async (req, res) => {
+  const {id} = req.body;
+  delete req.body.id;
+  await Movie.findByIdAndUpdate(id, req.body, {useFindAndModify: false});
+  console.log('req.bodyreq.bodyreq.bodyreq.bodyreq.bodyreq.body')
+  console.log(req.body)
+})
+
+app.put("/api/movies", jsonParser, async function (req, res) {
+
+  const {id} = req.body;
+  delete req.body.id;
+
+  if (!req.body) return res.sendStatus(400);
+
+  await Movie(id, req.body);
 });
+
+async function start() {
+  try {
+  const url = 'mongodb+srv://hero:123456qwerty@cluster0.qxjxl.mongodb.net/movies';
+
+  await mongoose.connect(url, {
+    useNewUrlParser: true,
+    useFindAndModify: false
+  });
+  app.listen(3000, function () {
+    console.log("Server is running on port: 3000");
+  });
+  } catch (e) {
+    console.log(e);
+  }
+
+}
+start();
