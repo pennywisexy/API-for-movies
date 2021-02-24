02cd492854b86249aaa0cedd18b7b4092d2589b3
Aliaksandr Snitko <pennywisexy@gmail.com>
2021-02-24 15:01:32 +0300

    remove hardcode

--- index.js
+++ index.js
@@ -7,16 +7,6 @@ const jsonParser = express.json();
 const Movie = require('./models/movie');
 const User = require('./models/user');
 
-app.use(async (req, res, next) => {
-  try {
-    const user = await User.findById('60350fceb85ea1641c8ff183');
-    req.user = user;
-    next();
-  } catch (e) {
-    console.log(e);
-  }
-})
-
 app.use(express.static(__dirname + "/public"));
 
 app.use(session({
@@ -140,18 +130,6 @@ async function start() {
     useNewUrlParser: true,
     useFindAndModify: false
   });
-
-  const candidate = await User.findOne();
-
-  if(!candidate) {
-    const user = new User ({
-      email: 'dddd@gmail.com',
-      password: '123456',
-      'first-name': 'Some',
-      'last-name': 'Name'
-    });
-    await user.save();
-  }
   app.listen(3000, function () {
     console.log("Server is running on port: 3000");
   });
