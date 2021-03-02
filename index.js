b1790d0c2121f1d8847a97ad4bbb1164ba87440f
Aliaksandr Snitko <pennywisexy@gmail.com>
2021-03-02 18:47:27 +0300

    added use method

--- index.js
+++ index.js
@@ -16,6 +16,18 @@ app.use(session({
   saveUninitialized: false
 }))
 
+app.use((req, res, nxt) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin, Content-Type, Accept, Accept-Language, Origin, User-Agent');
+    if(req.method === 'OPTIONS') {
+      res.sendStatus(204);
+    }
+    else {
+      nxt();
+    }
+})
+
 app.get("/api/movies", async function (req, res) {
   const movies = await Movie.find();
   res.send(movies);
